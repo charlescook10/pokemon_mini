8244d874b2619ee975a2275fe32df4a01721f41e
Charles Cook <charles.cook80@yahoo.co.uk>
2025-09-30 11:18:48 +0100

    added some moves

--- lib/moves.py
+++ lib/moves.py
@@ -1,3 +1,5 @@
+import pokemon_types
+
 # all moves will have a type and power=dmg done
 class Move():
     def __init__(self, name, type, power):
@@ -8,3 +10,7 @@ class Move():
         pokemon.take_dmg(self)
     def __str__(self):
         return f"{self.name} a {self.type} type move with {self.power} power."
+
+scratch = Move("Scratch", pokemon_types.normal, 40)
+quick_attack = Move("Quick Attack", pokemon_types.normal, 40)
+thunder_shock = Move("Thunder Shock", pokemon_types.electric, 40)
