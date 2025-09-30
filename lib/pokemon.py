f32a33b7a50dafcdbb23c18573e33f20fa7af1ab
Charles Cook <charles.cook80@yahoo.co.uk>
2025-09-30 15:53:09 +0100

    added notes and removed some methods from the sub class to be less DRY

--- lib/pokemon.py
+++ lib/pokemon.py
@@ -3,6 +3,8 @@ import pokemon_types
 import moves
 import random
 
+# This would be an example of an abstract class. Can not be instantiated. 
+
 class Pokemon(ABC):
     def __init__(self, species, types, move, name=False, hp=40, atk_pwr=1, defence=1, speed=1):
         self.species = species
@@ -45,33 +47,31 @@ class Pokemon(ABC):
         print(f"{self.name} has been healed!\n")
     def move(self):
         return self._learnt_move
+    def give_name(self, name):
+        self.name = name    
+    def attack(self, pokemon, dmg):
+        print(f"{self.name} uses {self._learnt_move.name} on {pokemon.name}!\n It did {dmg} damage!\n")
+    
     @abstractmethod
     def cry(self):
         # makes the pokemon species sound
         pass
-    @abstractmethod
-    def attack(self, pokemon, dmg):
-        pass
+
+# Pokemon is a parent class that the other classes inherit from.
 
 class Pikachu(Pokemon):
     def __init__(self, name=False):
         super().__init__("Pikachu", [pokemon_types.electric], random.choice([moves.quick_attack, moves.thunder_shock]), name)
+    # If cry did something then it would be an example of polymorphism
     def cry(self):
         pass
-    def attack(self, pokemon, dmg):
-        print(f"{self.name} uses {self._learnt_move.name} on {pokemon.name}!\n It did {dmg} damage!\n")
-    def give_name(self, name):
-        self.name = name
 
 class Charmander(Pokemon):
     def __init__(self, name=False):
         super().__init__("Charmander", [pokemon_types.fire], moves.scratch, name)
     def cry(self):
         pass
-    def attack(self, pokemon, dmg):
-        print(f"{self.name} uses {self._learnt_move.name} on {pokemon.name}!\n It did {dmg} damage!\n")
-    def give_name(self, name):
-        self.name = name
+    
 
 
 pokemon_list = [Charmander, Pikachu]
